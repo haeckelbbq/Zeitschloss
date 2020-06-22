7549ce71709ab01a96438745996356c3892d51ff
NicoW <NicoWoywod@gmx.de>
2020-06-22 15:57:39 +0200

    Test Commit immernoch Nico

---
+++ view/startseiteanzeige.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Zeitschloss</h1>
+<form method="post" action="index.php">
+
+    <input type="hidden" name="action" value="eingeloggt">
+
+    <input type="submit" name="submitname" value="OK">
+
+</form>
+
+</body>
+</html>
