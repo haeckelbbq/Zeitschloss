508f3d1a599c3b2f1e9525b09df597906d005455
NicoW <NicoWoywod@gmx.de>
2020-06-24 11:19:24 +0200

    com 2

--- index.php
+++ index.php
@@ -1,2 +1,48 @@
 <?php
-echo "Test 123";
+spl_autoload_register(function ($class){
+    include 'class/' . $class . '.php';
+});
+
+$view = '';
+$action = $_REQUEST['action'] ?? '';
+
+switch($action) {
+
+    case 'registrieren':
+    {
+
+        echo 'registrieren';
+
+        $area = 'registrieren';
+        $view =  $area . 'anzeige';
+        break;
+    }
+
+    case 'einloggen':
+    {
+
+        echo 'einloggen';
+
+        break;
+    }
+
+    case 'eingeloggt':
+    {
+
+        $view = 'startseiteanzeige';
+        echo 'eingeloggt';
+
+        break;
+    }
+
+    default:
+    {
+
+        $view = 'startseiteanzeige';
+        echo 'default';
+
+    }
+
+}
+
+include 'view\\' . $view . '.php';
